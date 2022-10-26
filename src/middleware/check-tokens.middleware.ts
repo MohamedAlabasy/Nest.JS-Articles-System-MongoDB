e05332db66917a194fe040d5a32509b0d1827e01
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-10-26 16:32:58 +0200

    fixed check tokens middleware

--- src/middleware/check-tokens.middleware.ts
+++ src/middleware/check-tokens.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware, ForbiddenException, UnauthorizedException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import * as jwt from 'jsonwebtoken';
 
@@ -8,29 +8,17 @@ import { ACCESS_TOKEN_SECRET } from '../config/token.config';
 @Injectable()
 export class CheckTokensMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    try {
-      const authHeader = req.headers["authorization"] || req.body.token || req.query.token;
-      const token = authHeader && authHeader.split(' ')[1]; //to get token without Bearer
-      if (!token) {
-        throw new Error("You must enter token");;
-      }
 
-      jwt.verify(token, ACCESS_TOKEN_SECRET as string, (error: any, decoded: any) => {
-        if (error) {
-          throw new Error('invalid token');
-        }
+    const authHeader = req.headers["authorization"] || req.body.token || req.query.token;
+    const token = authHeader && authHeader.split(' ')[1]; //to get token without Bearer
+    if (!token) throw new ForbiddenException("You must enter token");;
 
-        if (!decoded.is_verification) {
-          throw new Error('You must verification email first')
-        }
+    jwt.verify(token, ACCESS_TOKEN_SECRET as string, (error: any, decoded: any) => {
+      if (error) throw new UnauthorizedException('invalid token'); //No. (498 invalid token) is an unofficial code used by a proprietary product. 
 
-        next();
-      });
-    } catch (error) {
-      res.status(400).send({
-        statusCode: 400,
-        message: error.message
-      })
-    }
+      if (!decoded.is_verification) throw new ForbiddenException('You must verification email first')
+
+      next();
+    });
   }
 }
